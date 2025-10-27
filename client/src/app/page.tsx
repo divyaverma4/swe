2092cdfd71b3f27f5e7844dd4c58dc977946e375
Zhicheng Li <Zli40547@gmail.com>
2025-10-27 13:58:32 -0400

    Established frontend and backend connection

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,43 @@
 "use client";
 
 import { LoginScreen } from "@/components/login_screen";
+import { stat } from "fs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const checkConnection = async () => {
+    try {
+        const response = await fetch("http://localhost:5001/status");
+        if (!response.ok) {
+            console.error("Server responded with an error:", response.status);
+            return false;
+        }
+        return true;
+
+    } catch (error) {
+        console.error("Error during connection check:", error);
+        return false;
+    }
+};
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/login");
-  
-  return (
-    <div>
-    </div>
-  );
+    const router = useRouter();
+
+    useEffect(() => {
+        const runCheck = async () => {
+            const status = await checkConnection();
+            if (status === true) {
+                console.log("Redirecting to /login");
+                router.push("/login");
+            }
+        };
+
+        runCheck();
+
+    }, [router]);
+
+    return (
+        <div>Checking connection...</div>
+    );
 }
--- server/app.py
+++ server/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/status')
+def status():
+    print("Status endpoint was called.")
+    return jsonify({'status': 'ok'}), 200
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
